a30c40879f82da0595c0a338891e7457b1329dd0
Humza Ahmed <ShatteredPheonix@eroam-158-227.wifi.wmin.ac.uk>
2015-01-22 10:03:52 +0000

    Initial Commit

---
+++ IMix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IMix
+//
+//  Created by Humza Ahmed on 22/01/2015.
+//  Copyright (c) 2015 Humza Ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IMix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IMix
+//
+//  Created by Humza Ahmed on 22/01/2015.
+//  Copyright (c) 2015 Humza Ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
