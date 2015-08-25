63fe0d409a1c18d4ab68695418957576b60eba48
Humza <hahmed2007@gmail.com>
2015-08-25 15:16:10 +0100

    Open Source License
    Adding Open Source License Declaration to all Files

--- IMix/AppDelegate.m
+++ IMix/AppDelegate.m
@@ -6,6 +6,24 @@
 //  Copyright (c) 2015 Humza Ahmed. All rights reserved.
 //
 
+/***********************************************************************
+ IMix - Simple a colour palette to combine and mix a variety of colours through different channels.
+ Copyright (C) 2015 Humza Ahmed
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ***********************************************************************/
+
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
